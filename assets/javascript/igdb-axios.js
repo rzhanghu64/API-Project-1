bbf0a4bec3aa8130b19f296b165f1fb48b95d26c
Henry C <hchen651@gmail.com>
2019-03-02 13:11:34 -0500

    changed call

--- assets/javascript/igdb-axios.js
+++ assets/javascript/igdb-axios.js
@@ -1,5 +1,5 @@
 axios({
-    url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
+    url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers",
     method: 'POST',
     headers: {
         'Accept': 'application/json',
@@ -7,7 +7,7 @@ axios({
     },
 
     //https://api-docs.igdb.com/?javascript#examples-12 and https://api-docs.igdb.com/?javascript#game
-    data: "fields *; where genres = (8) & platforms = 48;"
+    data: "fields *; where game = 104673;"
 })
     .then(response => {
         console.log(response.data);
