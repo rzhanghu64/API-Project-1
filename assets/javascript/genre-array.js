b0c7a90d08df1502f342fcd4a136513f0dbdc259
Henry <hchen651@gmail.com>
2019-03-01 21:44:27 -0500

    attempting to fix api call

--- assets/javascript/genre-array.js
+++ assets/javascript/genre-array.js
@@ -1,23 +1,26 @@
-var queryURL = "https://www.giantbomb.com/api/genres/?api_key=3e367e43b48af015b21cb7640630f3fa0e510098";
 var dropdown = [];
 var dict = {
     name: "",
     guid: "",
-  };
-
-$.ajax({
-    url: queryURL,
-    dataType: "jsonp",
-    jsonp: 'json_callback',
-    method: "GET",
-})
-    .then(function (response) {
-        console.log("hey");
-        var results = response.docs;
-        for (var i = 0; i < results.length; i++) {
-            var d = new dict;
-            d.name =  results.name;
-            d.guid = results.guid;
-            dropdown.push(d);
-        }
-    });
+};
+$("#clickhead").click(function () {
+    $.ajax({
+        url: "https://www.giantbomb.com/api/genre/",
+        dataType: "jsonp",
+        jsonp: 'json_callback',
+        data: {
+            api_key: '3e367e43b48af015b21cb7640630f3fa0e510098',
+            format: 'jsonp',
+        },
+    })
+        .then(function (response) {
+            console.log(response);
+            var results = response.docs;
+            for (var i = 0; i < results.length; i++) {
+                var d = new dict;
+                d.name = results.name;
+                d.guid = results.guid;
+                dropdown.push(d);
+            }
+        });
+});
