2154d9d4415d024d93e75379ed5f77b19820b92a
Henry C <hchen651@gmail.com>
2019-03-01 20:19:50 -0500

    attempting to fix gb api calls

--- assets/javascript/genre-array.js
+++ assets/javascript/genre-array.js
@@ -1,1 +1,23 @@
-var queryURL = "https://www.giantbomb.com/api/genres/?format=json&api_key=3e367e43b48af015b21cb7640630f3fa0e510098";
+var queryURL = "https://www.giantbomb.com/api/genres/?api_key=3e367e43b48af015b21cb7640630f3fa0e510098";
+var dropdown = [];
+var dict = {
+    name: "",
+    guid: "",
+  };
+
+$.ajax({
+    url: queryURL,
+    dataType: "jsonp",
+    jsonp: 'json_callback',
+    method: "GET",
+})
+    .then(function (response) {
+        console.log("hey");
+        var results = response.docs;
+        for (var i = 0; i < results.length; i++) {
+            var d = new dict;
+            d.name =  results.name;
+            d.guid = results.guid;
+            dropdown.push(d);
+        }
+    });
