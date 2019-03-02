99c5d5923fb9ac7889246217fe9aea1419b440bc
Richard Hu <46357823+rzhanghu64@user.noreply.github.com>
2019-03-02 12:06:27 -0500

    working game and genre filter

--- assets/javascript/igdb-axios.js
+++ assets/javascript/igdb-axios.js
@@ -15,3 +15,20 @@ axios({
     .catch(err => {
         console.error(err);
     });
+
+axios({
+    url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/genres",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'user-key': '9e200e5f3ba806bf8825821dd078350c',
+    },
+    //https://api-docs.igdb.com/?javascript#examples-12
+    data: "fields *; limit 40;"
+})
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
