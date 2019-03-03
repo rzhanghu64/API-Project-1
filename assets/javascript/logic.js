0b650b11228903e6f5f0186bb7b95bde1ca8a498
Henry <hchen651@gmail.com>
2019-03-02 23:35:14 -0500

    changed home and logic, added placeholders

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+async function initialCall(){
 axios({
     url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
     method: 'POST',
@@ -9,20 +10,24 @@ axios({
     //https://api-docs.igdb.com/?javascript#examples-12 and https://api-docs.igdb.com/?javascript#game
     data: "fields *; where genres = (8) & platforms = 48;"
 })
-    .then(response => {
+    .then(async response => {
         var results = response.data;
         console.log(response.data);
         for (var i = 0; i < 4; i++) {
             var gameID = results[i].id;
             var cardDiv = $('<div>');
             cardDiv.addClass("card");
+            cardDiv.addClass("col-3");
             var name = results[i].name;
             var h = $("<h5>").text(name);
             h.addClass("card-title");
             var img = $("<img>");
             img.addClass("game-image");
-            console.log(getCover(gameID));
-            img.attr("src", getCover(gameID));
+            img.data("gameID", gameID);
+            var imgLocation = "https://via.placeholder.com/100";
+            //await getCover(gameID);
+            //console.log(imgLocation);
+            img.attr("src", imgLocation);
             cardDiv.append(img);
             cardDiv.append(h);
             $("#card-container").prepend(cardDiv);
@@ -31,8 +36,10 @@ axios({
     .catch(err => {
         console.error(err);
     });
+};
+initialCall();
 
-function getCover(id) {
+async function getCover(id) {
     axios({
         url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers",
         method: 'POST',
@@ -45,9 +52,8 @@ function getCover(id) {
         data: "fields *; where game = " + id + ";"
     })
         .then(response => {
-            console.log(response.data);
             var results = response.data;
-            return results[0].url;
+            imgLocation = results[0].url;
         })
         .catch(err => {
             console.error(err);
