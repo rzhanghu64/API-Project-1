4c2a083477dacfecb7f4f90fc800a2b22b17244a
Henry <hchen651@gmail.com>
2019-03-05 00:32:34 -0500

    fixed nav menu, added new img api call

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,5 @@
+var imgsrc;
+
 async function initialCall(){
 axios({
     url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
@@ -14,31 +16,43 @@ axios({
         var results = response.data;
         console.log(response.data);
         for (var i = 0; i < 4; i++) {
-            var gameID = results[i].id;
             var cardDiv = $('<div>');
             cardDiv.addClass("card");
+            cardDiv.attr("id", "card-div-"+i)
             cardDiv.addClass("col-3");
             var h = $("<h5>").text(results[i].name);
             h.addClass("card-title");
             var p = $("<p>").text(results[i].summary)
             var img = $("<img>");
             img.addClass("game-image");
-            img.data("gameID", gameID);
-            var imgLocation = "https://via.placeholder.com/100";
+            img.attr("id", "game-image-"+i)
+            img.attr("data-gameid", results[i].id);
             //await getCover(gameID);
             //console.log(imgLocation);
-            img.attr("src", imgLocation);
+            img.attr("src", "https://via.placeholder.com/100");
             cardDiv.append(img);
             cardDiv.append(h);
             cardDiv.append(p);
             $("#card-container").append(cardDiv);
         }
+        fetchCovers();
     })
     .catch(err => {
         console.error(err);
     });
 };
 initialCall();
+
+async function fetchCovers(){
+    for (var i = 0; i < 1; i++) {
+        var img = $("#game-image-"+i);
+        var currentGameID = img.data("gameid");
+        var imgsrc = await getCover(currentGameID);
+        console.log(imgsrc);
+        img.attr("src", imgsrc);
+        $("#card-div-"+i).prepend(img);
+    }
+};
 
 async function getCover(id) {
     axios({
@@ -54,7 +68,9 @@ async function getCover(id) {
     })
         .then(response => {
             var results = response.data;
-            imgLocation = results[0].url;
+            console.log(results[0].url);
+            imgsrc = results[0].url;
+            return results[0].url;
         })
         .catch(err => {
             console.error(err);
