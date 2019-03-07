7c88c82622bd564c61e79be89898a9728dad25d9
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-03-07 14:31:21 -0500

    copied test into logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -93,3 +93,14 @@ function getCover(id) {
 // }).fail(function () {
 //     alert("ajax error");
 // });
+
+// Google CSE API test
+var queryURL = "https://www.googleapis.com/customsearch/v1?q=" + "game" + "&searchType=image&cx=015084081056955684922:ejzkcmko2ki&key=AIzaSyDMAd3zOHJzfhAWHpweYkdtIunLkpq6s5U";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+        console.log(response)
+    });
